aae95d31211dec0100216c1eb4123f0d3a3a5007
chava93 <salvador.cmenendez@gmail.com>
2024-08-06 12:03:00 -0600

    fix routes sector foreignkey reference

--- encadenes/models.py
+++ encadenes/models.py
@@ -30,7 +30,7 @@ class Route(models.Model):
 	rating = models.DecimalField(max_digits=3, decimal_places=2, default=0)
 	total_ascents = models.IntegerField(default=0)
 	recommended_by = models.IntegerField(default=0)
-	sector = models.ForeignKey(Crag, on_delete=models.CASCADE)
+	sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
 	grado = models.CharField(max_length=10, default='000')
 	def __str__(self):
 		return self.route
