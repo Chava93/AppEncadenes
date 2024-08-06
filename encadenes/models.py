3f25412a8cfdc382b3dd0db843d71ea9f56998fa
chava93 <salvador.cmenendez@gmail.com>
2024-08-06 11:41:54 -0600

    add str methods to db classes

--- encadenes/models.py
+++ encadenes/models.py
@@ -3,19 +3,27 @@ from django.utils import timezone
 
 class Country(models.Model):
 	country = models.CharField(max_length=100, unique=True)
+	def __str__(self):
+		return self.country
 
 class States(models.Model):
 	state = models.CharField(max_length=100)
 	country = models.ForeignKey(Country, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.States
 
 class Crags(models.Model):
 	crag  = models.CharField(max_length=100, unique=True)
 	state = models.ForeignKey(States, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.crag
 
 class Sectors(models.Model):
 	sector = models.CharField(max_length=100, unique=True)
 	crag = models.ForeignKey(Crags, on_delete=models.CASCADE)
 	total_routes = models.IntegerField(default=0)
+	def __str__(self):
+		return self.sector
 
 class Routes(models.Model):
 	route = models.CharField(max_length=100, unique=True)
@@ -24,3 +32,5 @@ class Routes(models.Model):
 	recommended_by = models.IntegerField(default=0)
 	sector = models.ForeignKey(Crags, on_delete=models.CASCADE)
 	grado = models.CharField(max_length=10, default='000')
+	def __str__(self):
+		return self.route
