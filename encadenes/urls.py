26d361296d7b04ee1666f853c90538b0dcb3d768
chava93 <salvador.cmenendez@gmail.com>
2024-08-06 13:06:35 -0600

    Add Views to check DB content

--- encadenes/urls.py
+++ encadenes/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
      # ex: /encadenes/
     path("", views.mainView, name="index"),
     # ex: /encadenes/crags
-    path("crags/", views.CragsView.as_view(), name="crags")
+    path("crags/", views.CragsView.as_view(), name="crags"),
+    path("routes/", views.RoutesView.as_view(), name="routes"),
+    path("sectors/", views.SectorsView.as_view(), name="sectors"),
 ]
--- encadenes/views.py
+++ encadenes/views.py
@@ -11,8 +11,30 @@ def mainView(request):
 class RoutesView(generic.ListView):
 	template_name = "encadenes/ListView.html"
 	model = Route
+	title = "Rutas"
+	def get_context_data(self, **kwargs):
+	    # Call the base implementation first to get a context
+	    context = super().get_context_data(**kwargs)
+	    context["title"] = self.title
+	    return context
 
 class CragsView(generic.ListView):
 	template_name = "encadenes/ListView.html"
 	model = Crag
+	title = "Crags"
+	def get_context_data(self, **kwargs):
+	    # Call the base implementation first to get a context
+	    context = super().get_context_data(**kwargs)
+	    context["title"] = self.title
+	    return context
+
+class SectorsView(generic.ListView):
+	template_name = "encadenes/ListView.html"
+	model = Sector
+	title = "Sectors"
+	def get_context_data(self, **kwargs):
+	    # Call the base implementation first to get a context
+	    context = super().get_context_data(**kwargs)
+	    context["title"] = self.title
+	    return context
 		
