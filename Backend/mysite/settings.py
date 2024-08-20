a4857b40e2588e3ae4c5364b70c672c3534f00ca
Rodrigo Sánchez Isunza <rodrigoisunza@gmail.com>
2024-08-20 16:27:00 -0600

    Add Django Rest Framework

--- Backend/mysite/settings.py
+++ Backend/mysite/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "encadenes.apps.EncadenesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "encadenes.apps.EncadenesConfig",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
